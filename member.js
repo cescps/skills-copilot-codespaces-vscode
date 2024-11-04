675c0f16ed0a7374048aed5293dc67b2312ba884
Francesc Pinyol Santsalvador <83516572+cescps@users.noreply.github.com>
2024-11-04 12:40:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  return {
+    "skills": [
+      {
+        "name": "JavaScript",
+        "level": "intermediate"
+      },
+      {
+        "name": "HTML",
+        "level": "advanced"
+      },
+      {
+        "name": "CSS",
+        "level": "advanced"
+      },
+      {
+        "name": "Node.js",
+        "level": "beginner"
+      }
+    ]
+  }
+}
